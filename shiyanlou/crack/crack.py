128650aeefab290313aecaddcbb603cc66b28013
dronly <dronly@outlook.com>
2016-11-24 22:34:18 +0800

    python识别验证码

---
+++ shiyanlou/crack/crack.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+# 识别图片验证码。
+from PIL import Image
+import hashlib
+import time
+import os
+
+import math
+
+class VectorCompare:
+    def magnitude(self, concordance):
+        total = 0
+        for word,count in concordance.items():
+            total += count ** 2
+        return math.sqrt(total)
+
+    def relation(self, concordance1, concordance2):
+        relevance = 0
+        topvalue = 0
+        for word, count in concordance1.items():
+            if word in concordance2:#concordance2.has_key(word):
+                topvalue += count * concordance2[word]
+        return topvalue / (self.magnitude(concordance1) * self.magnitude(concordance2))
+
+def buildvector(im):
+    d1 = {}
+    count = 0
+    for i in im.getdata():
+        d1[count] = i
+        count += 1
+    return d1
+
+v = VectorCompare()
+
+iconset = ['0','1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+imageset = []
+
+for letter in iconset:
+#    print(letter)
+#    print(os.listdir('./iconset/%s/' % (letter)))
+    for img in os.listdir('./iconset/%s/'%(letter)):
+        temp = []
+        if img != 'Thumbs.db' and img != '.DS_Store':
+            temp.append(buildvector(Image.open('./iconset/%s/%s' %(letter, img))))
+        imageset.append({letter:temp})
+
+#print(imageset)
+im = Image.open("captcha.gif")
+#将图片转换为8位像素模式
+im = im.convert("P")
+im2 = Image.new("P", im.size, 255)
+temp = {}
+# 打印颜色直方图
+#print(im.histogram())
+
+
+for x in range(im.size[1]):
+    for y in range(im.size[0]):
+        pix = im.getpixel((y,x))
+        temp[pix] = pix
+        if pix == 220 or pix == 227:
+            im2.putpixel((y,x), 0)
+
+inletter = False
+foundletter = False
+start = 0
+end = 0
+
+letters = []
+
+for y in range(im2.size[0]):
+    for x in range(im2.size[1]):
+        pix = im2.getpixel((y,x))
+        if pix != 255:
+            inletter = True
+    if foundletter == False and inletter == True:
+        foundletter = True
+        start = y
+    if foundletter == True and inletter == False:
+        foundletter = False
+        end = y
+        letters.append((start, end))
+    inletter = False
+
+count = 0
+
+for letter in letters:
+    m = hashlib.md5()
+    im3 = im2.crop((letter[0], 0, letter[1], im2.size[1]))
+    guess = []
+    for image in imageset:
+        #print (image)
+        for x,y in image.items():
+            if len(y) != 0:
+                guess.append((v.relation(y[0], buildvector(im3)), x))
+    guess.sort(reverse=True)
+    print(guess[0])
+    #print ("", guess[0])
+    count += 1
+
+'''
+his = im.histogram()
+values = {}
+
+for i in range(256):
+    values[i] = his[i]
+
+for j,k in sorted(values.items(), key=lambda x:x[1], reverse=True)[:10]:
+    print(j, k)
+
+
+im2.show()
+'''
