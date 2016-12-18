fc869ab4d442519c2716775a7e54c658156f4546
dronly <dronly@outlook.com>
2016-12-18 22:36:15 +0800

    隐写术添加注释-1

--- shiyanlou/steganography/steganography.py
+++ shiyanlou/steganography/steganography.py
@@ -4,27 +4,37 @@
 from PIL import Image
 
 def makeImageEven(image):
-	pixels = list(image.getdata())
-	evenPixels = [(r>>1<<1, g>>1<<1, b>>1<<1, t>>1<<1) for [r, g, b, t] in pixels]
-	evenImage = Image.new(image.mode, image.size)
-	evenImage.putdata(evenPixels)
+	pixels = list(image.getdata()) # 获得 image 的像素信息列表
+	evenPixels = [(r>>1<<1, g>>1<<1, b>>1<<1, t>>1<<1) for [r, g, b, t] in pixels] # 通过位运算，将 pixels 每个元素的最后一位设为0
+	evenImage = Image.new(image.mode, image.size) # 创建一个与 image 相同大小相同格式的图片
+	evenImage.putdata(evenPixels) # 给 evenImage 添加像素信息。
 	return evenImage
 
 
+	"""	
+	这个函数就是将以'ob'开头的不等长二进制数转化为8位二进制数
+	"""
 def  constLenBin(int):
-	binary = "0" * (8-(len(bin(int))-2)) + bin(int).replace('0b', '')
+	binary = "0" * (8-(len(bin(int))-2)) + bin(int).replace('0b', '') # bin 函数 讲整形数字转换成二进制形式， 
 	return binary
 
+	"""
+	把数据存入到image中。
+	"""
 def  encodeDataInImage(image, data):
-	evenImage = makeImageEven(image)
-	binary = ''.join(map(constLenBin, bytearray(data, 'utf-8')))
-	if len(binary) > len(image.getdata()) * 4:
+	evenImage = makeImageEven(image) # 把参数 image 每个像素点的最后以为改成0
+	binary = ''.join(map(constLenBin, bytearray(data, 'utf-8'))) # map 高阶函数，第一个参数传入函数 constLenBin 第二个参数传入 list 
+	if len(binary) > len(image.getdata()) * 4: # 如果 binary 长度大于 image.getdata()*4 说明需要存入的数据太大， image 不能写下
 		raise Exception("Error: Can't encode more than" + len(evenImage.getdata()) * 4 + " bits in this image. ")
-	encodedPixels = [(r+int(binary[index*4+0]), g+int(binary[index*4+1]), b+int(binary[index*4+2]), t+int(binary[index*4+3]))if index*4 < len(binary) else (r, g, b, t) for index, (r,g,b,t) in enumerate(list(evenImage.getdata()))]
-	encodedImage = Image.new(evenImage.mode, evenImage.size)
+	# 这一长句的作用就是把 binary 中的每一位依次放入到 evenImage.getdata() 中的 rgbt 值的最后一位。用到了列表生成器， enumerate 函数--》同时列表索引以及值。
+	encodedPixels = [(r+int(binary[index*4+0]), g+int(binary[index*4+1]), b+int(binary[index*4+2]), t+int(binary[index*4+3]))if index*4 < len(binary) else (r, g, b, t) for index, (r,g,b,t) in enumerate(list(evenImage.getdata()))] 
+	encodedImage = Image.new(evenImage.mode, evenImage.size) 
 	encodedImage.putdata(encodedPixels)
 	return encodedImage
 
+	"""
+	将二进制数据转化位 str
+	"""
 def binaryToString(binary):
 	index = 0
 	string = []
@@ -37,6 +47,9 @@ def binaryToString(binary):
 		index += length
 	return ''.join(string)
 
+	"""
+	把 image 图片中隐藏的数据拿出来。
+	"""
 def decodeImage(image):
 	pixels = list(image.getdata())
 	binary = ''.join([str(int(r>>1<<1!=r))+str(int(g>>1<<1!=g))+str(int(b>>1<<1!=b))+str(int(t>>1<<1!=t)) for (r,g,b,t) in pixels])
@@ -47,18 +60,3 @@ def decodeImage(image):
 
 encodeDataInImage(Image.open("coffee.png"), '你好世界，Hello world!').save('encodeImage.png')
 print(decodeImage(Image.open("encodeImage.png")))
-
-#image1 = Image.open("coffee.png")
-#image1.show()
-
-#image2 = makeImageEvent(image1)
-#image2.show()
-
-
-
-'''
-def encodeDataInImage(image, data):
-	evenImage = makeImageEvent(image)
-	binary = ''.join(map(const))
-'''
-
