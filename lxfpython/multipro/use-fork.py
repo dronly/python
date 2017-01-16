89180673781b3987169a64189dca880878fe77d5
dronly <dronly@outlook.com>
2017-01-16 09:12:23 +0800

    整理

--- lxfpython/multipro/use-fork.py
+++ lxfpython/multipro/use-fork.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os
 print('Process (%s) start' % os.getpid())
 
 pid = os.fork()
-if pid==0:
-	print('I am child process(%s) adn my parent is %s.' % (os.getpid(), os.getppid()))
+if pid == 0:
+    print('I am child process(%s) adn my parent is %s.' % (os.getpid(), os.getppid()))
 else:
-	print ('I (%s) just created a child process (%s).' % (os.getpid(), pid))
+    print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
--- lxfpython/multipro/use-miltipro.py
+++ lxfpython/multipro/use-miltipro.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-#! -*- coding: utf-8 -*-
+# ! -*- coding: utf-8 -*-
 from multiprocessing import Process
 import os
 
-#子进程要执行的代码
-def run_proc(name):
-	print('Run child process %s (%s)...' % (name, os.getpid()))
+# 子进程要执行的代码
 
-if __name__=='__main__':
-	print('Parent process %s.' % os.getpid())
-	p = Process(target=run_proc, args=('test',))
-	print('Child process wll start.')
-	p.start()
-	p.join()
-	print('Child proess end.')
+
+def run_proc(name):
+    print('Run child process %s (%s)...' % (name, os.getpid()))
+
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process wll start.')
+    p.start()
+    p.join()
+    print('Child proess end.')
