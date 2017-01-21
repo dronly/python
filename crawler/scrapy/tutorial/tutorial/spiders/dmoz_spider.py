18c5a80c8403a1cf26198f31a0f274793bbcea04
dronly <dronly@outlook.com>
2017-01-21 15:01:29 +0800

    scrapy study

---
+++ crawler/scrapy/tutorial/tutorial/spiders/dmoz_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class DmoSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        # filename = response.url.split("/")[-2] + '.html'
+        # print(response.url)
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        for sel in response.xpath('//ul/li'):
+            title = sel.xpath('a/text()').extract()
+            link = sel.xpath('a/@href').extract()
+            desc = sel.xpath('text()').extract()
+            print(title, link, desc)
