5754303ee4c6dbf7b1891f186b16d68893090a47
dronly <dronly@outlook.com>
2017-02-20 16:45:20 +0800

    Flask-script 命令行 2c

--- web/flask-web-dev/hello.py
+++ web/flask-web-dev/hello.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import request
+from flask_script import Manager
 
 app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route("/")
@@ -17,5 +19,5 @@ def user(name):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    # app.run(debug=True)
+    manager.run()
