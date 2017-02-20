4caa2c362c4595cedb1df20ec62a6ba70d82160b
dronly <dronly@outlook.com>
2017-02-20 17:15:53 +0800

    Jinja2 模板 3a

--- web/flask-web-dev/hello.py
+++ web/flask-web-dev/hello.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request
+from flask import render_template
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -8,16 +8,14 @@ manager = Manager(app)
 
 @app.route("/")
 def index():
-    user_agent = request.headers.get('User-Agent')
-    # return 'Hello World!'
-    return '<p>Your browser is %s</p>' % user_agent
+    return render_template('index.html')
 
 
 @app.route("/user/<name>")
 def user(name):
-    return 'Hello, %s!' % name
+    return render_template('user.html', name=name)
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    manager.run()
+    app.run(debug=True)
+    #manager.run()
