148c892f00814afe2aadf41bd56bedb07faba506
dronly <dronly@outlook.com>
2017-02-21 01:24:05 +0800

    web 表单 4c

--- web/flask-web-dev/hello.py
+++ web/flask-web-dev/hello.py
@@ -1,14 +1,23 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask,render_template, session, redirect, url_for, flash
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard go guess string'
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(Form):
+    name = StringField('What is your name', validators=[Required()])
+    submit = SubmitField('Submit')
 
 
 @app.errorhandler(404)
@@ -21,9 +30,16 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit() is True:
+        oldname = session.get('name')
+        if oldname is not None and oldname != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.route("/user/<name>")
